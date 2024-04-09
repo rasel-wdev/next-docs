5292ddba5260c3dba5602fc254e9dc1db7e8c13c
Md. Rasel Rana <rasel.learn22@gmail.com>
2024-04-10 05:56:23 +0600

    The docs file location is changed

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { getDocuments } from "@/lib/doc";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -9,6 +10,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  getDocuments();
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
